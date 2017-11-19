547a054b6534d05b7d834b1fef855394f740dfa3
unknown <brandon.geeman.lee@gmail.com>
2017-11-19 14:39:42 -0800

    added api functions to get an article form mongo, delete an article from mongo, and save an article to mongo

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -2,8 +2,23 @@ import axios from 'axios';
 
 const api = {
 
-  search: function(topic, startYear, endYear) {
+  search:(topic, startYear, endYear) => {
     const APIkey = 'dd5006dd525842fd8a6eb8360611378a'
     const queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' + APIkey + '&q=' + topic + '&begin_date=' + startYear + '0101&end_date' + endYear +'0101';
+    return axios.get('queryURL');
   },
-}
+
+  getArticle: () => {
+    return axios.get('/api/saved');
+  },
+
+  saveArticle: articleObj => {
+    return axios.post('/api/saved', articleObj);
+  }
+
+  deleteArticle: id => {
+    return axios.delete(`/api/save/${id}`);
+  }
+};
+
+export default api;
