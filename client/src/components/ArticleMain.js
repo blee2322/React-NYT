6c4ca4c75f11f6106327c8be0877691e7af2dbb6
unknown <brandon.geeman.lee@gmail.com>
2017-11-19 18:39:12 -0800

    adding event handlers to ArticleMain.js file

--- client/src/components/ArticleMain.js
+++ client/src/components/ArticleMain.js
@@ -14,7 +14,7 @@ class ArticleMain extends Component {
     saved: []
   };
 
-  componentDidMount() {
+  componentDidMount = () => {
     this.getSavedArticles()
   }
 
@@ -39,5 +39,52 @@ class ArticleMain extends Component {
     ));
   }
 
-  
+  renderSaved = () => {
+    return this.state.articles.map(article => (
+      <ArticleSaved
+          _id={article._id}
+          key={article._id}
+          title={article.headline.main}
+          date={article.pub_date}
+          url={article.web_url}
+          handleDeleteButton={this.handleDeleteButton}
+          getsavedArticles={this.getSavedArticles}
+      />
+    ));
+  }
+
+  handleTopicChange = (event) => {
+    this.setState({ topic: event.target.value });
+  }
+
+  handleStartYearChange = (event) => {
+    this.setState({ startYear: event.target.value });
+  }
+
+  handleEndYearChange = (event) => {
+    this.setState({ endYear: event.target.value });
+  }
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    API.search(this.state.topic, this.state.startYear, this.state.endYear)
+    .then((res) => {
+      this.setState({ articles: res.data.response })
+    })
+  }
+
+  handleSaveButton = (id) => {
+    const findArticleById = this.state.articles.find((add) => add._id === id);
+    const saveNewArt = {title: findArticleById.headline.main, date: findArticleById.pub_date, url: findArticleById.web_url};
+    API.saveArticle(saveNewArt)
+    .then(this.getSavedArticles());
+  }
+
+  handleDeleteButton = (id) => {
+    API.deleteArticle(id)
+      .then(this.getSavedArticles());
+  }
+
+  render()
+
 }
